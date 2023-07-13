9bc0325da165a15b8c2e5af59cb3a5cec60ed0ef
md-siam07 <dynamicsiam01@gmail.com>
2023-07-13 13:41:50 +0600

    chatgpt created

--- GPTBackend/routes/index.router.js
+++ GPTBackend/routes/index.router.js
@@ -4,10 +4,11 @@ const multer = require('multer');
 
 const statusController = require('../controllers/status.controller');
 const speechToTextController = require('../controllers/text-speech.controller')
-
+const gptController = require('../controllers/gpt.controller')
 
 router.post('/v1/status', statusController.createStatus);
 router.get('/v1/status', statusController.getStatus);
 router.post('/v1/speech2text', speechToTextController.getTextFromSpeech);
+router.get('/v1/prompt', gptController.getPrompt )
 
 module.exports = router;
