3e7ad768e8d987f8d4e77b751ab65b2466a89c02
Dardaul Hoque <rudro1152@gmail.com>
2023-07-13 15:38:30 +0600

    rename route

--- GPTBackend/routes/index.router.js
+++ GPTBackend/routes/index.router.js
@@ -1,13 +1,12 @@
-const express = require('express');
-const router = express.Router();
-const multer = require('multer');
+const express = require("express")
+const router = express.Router()
+const multer = require("multer")
 
-const statusController = require('../controllers/status.controller');
-const speechToTextController = require('../controllers/text-speech.controller')
+const statusController = require("../controllers/status.controller")
+const speechToTextController = require("../controllers/text-speech.controller")
 
+router.post("/v1/status", statusController.createStatus)
+router.get("/v1/status", statusController.getStatus)
+router.get("/v1/omnigpt", speechToTextController.getSpeechFromText)
 
-router.post('/v1/status', statusController.createStatus);
-router.get('/v1/status', statusController.getStatus);
-router.post('/v1/speech2text', speechToTextController.getTextFromSpeech);
-
-module.exports = router;
+module.exports = router
