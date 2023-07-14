e6becac9225ac0a7cfb583e7beb28fd39b21f1e2
Shafiq Saleheen <bsse1125@iit.du.ac.bd>
2023-07-14 07:29:08 +0600

    ui done

--- front-end/app/page.tsx
+++ front-end/app/page.tsx
@@ -1,13 +1,17 @@
+'use client'
 import NavBar from "@/components/navigation/nav-bar"
 import PdfShare from "@/components/pdf-sharing/pdf-sharing"
 import PlatformPdf from "@/components/platform-pdf/PlatformPdf"
+import StatusView from "@/components/platform-pdf/StatusView"
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center w-full">
       <NavBar />
       <div className="my-5" />
+      <PdfShare />
       <PlatformPdf />
+      <StatusView />
     </div>
   )
 }
